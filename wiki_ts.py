6f188285ea8aa6dec6485492a5f370f5d17ee4f3
Benjamin Ricaud <benjamin.ricaud@gmail.com>
2018-08-16 12:18:18 +0200

    investigate pagelinks

--- wiki_ts.py
+++ wiki_ts.py
@@ -10,21 +10,21 @@ import pandas as pd
 
 def read_data(filename):
 	wrapper = codecs.getreader('utf-8')
-	linefilter = re.compile("(\S+)")
+	#linefilter = re.compile("(\S+)")
 	print('Extracting data from',filename)	
 	with bz2.open(filename, 'rb') as f:
 		wf = wrapper(f,errors='replace')
-		count = 0
+		#count = 0
 		page_list = []
 		for line in tqdm(wf):
-			count += 1
-			info_list = linefilter.findall(line)
-			if info_list[0] == 'en.z': # english wikipedia prefix
+			#count += 1
+			#info_list = linefilter.findall(line)
+			#info_list = line.split()
+			#if info_list[0] == 'en.z': # english wikipedia prefix
+			#	page_list.append(info_list[1:])
+			if line.startswith('en.z'): # english wikipedia prefix
+				info_list = line.split()
 				page_list.append(info_list[1:])
-				#print(line)
-				#print(info_list)
-			#if (count % 100000) == 0:
-			#    print(count)
 	print('Converting to Dataframe')
 	df=pd.DataFrame(page_list,columns=['Title','DailyTotal','EncodedTimeseries'])
 	df.DailyTotal = df.DailyTotal.astype(int)
@@ -40,6 +40,8 @@ def decode_ts(df,visit_threshold):
 	for row in tqdm(df.itertuples()):
 		if int(row.DailyTotal) >= visit_threshold:
 			row_idx = row.Index
+			# The encoded timeseries contains pieces
+			# starting with one letter (hour of the day), followed by a number (visits) 
 			hourly_sliced = re.findall('\w\d+',row.EncodedTimeseries)
 			for hour_slice in hourly_sliced:
 				ts_df.loc[row_idx, hour_slice[0]] = int(hour_slice[1:])
@@ -52,6 +54,23 @@ def merge_pages_and_ts(df,df_ts):
 	result = result.sort_values('DailyTotal', ascending=False)
 	return result
 
+def get_info_from_filename(filename):
+	folder,fname = os.path.split(filename)
+	date = fname[11:-4]
+	#print('Date of file:',date)
+	return folder, fname, date
+
+def extract_timeseries(filename,min_visits_per_day):
+	data_df = read_data(filename)
+	df_ts = decode_ts(data_df,min_visits_per_day)
+	full_data = merge_pages_and_ts(data_df,df_ts)
+	# Saving to csv file
+	folder, fname, date = get_info_from_filename(filename)
+	csv_file = 'timeseries-' + date + '-T' + str(min_visits_per_day) + '.csv'
+	csv_path = os.path.join(folder,csv_file)
+	full_data.to_csv(csv_path,index=False)
+	return full_data
 
 if __name__ == "__main__":
-	print('Module for extracting the number of visits per page per hour recorded by Wikimedia')
+	print('Module for extracting the number of visits per page per hour'
+		+ ' recorded by Wikimedia')
